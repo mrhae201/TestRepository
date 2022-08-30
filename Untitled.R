3d93dfd178594b2df50282f59d04d3a346554246
mrhae201 <haggardm@vcu.edu>
2022-08-30 13:17:13 -0400

    hgjfyugh

---
+++ Untitled.R
@@ -0,0 +1,11 @@
+library(usethis)
+create_github_token()
+credentials::set_github_pat("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")
+
+library(credentials)
+set_github_pat("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")
+
+
+git_sitrep()
+
+gitcreds::gitcreds_set()
